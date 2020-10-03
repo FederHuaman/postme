94d08ada5d1ce959f6c688809464e5f780b012ca
feder huaman aquise <feder.huaman@hotmail.com>
2020-10-03 18:55:41 -0500

    Capturando el evento de install y se asigna al boton del banner

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ let MAIN;
 let MODAL_POST;
 let BTN_SHOW_POST;
 let BTN_CANCEL_POST;
-
+let defferedPrompt;
 // Funciones
 const showPostModal = () => {
     MAIN.style.display = 'none';
@@ -20,6 +20,14 @@ const showPostModal = () => {
     MAIN.style.display = 'block';
     MODAL_POST.style.transform = 'translateY(100vh)';
   };
+
+
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    console.log('Anulado el prompt')
+    e.preventDefault();
+    defferedPrompt = e;
+  })
 
 // Cuando se cargue todo nuestro DOM
 
@@ -39,5 +47,16 @@ window.addEventListener('load', async () => {
         }
       }
     }
+
+    const bannerInstall = document.getElementById('banner-install');
+    bannerInstall.addEventListener('click', async() => {
+      if(defferedPrompt){
+        defferedPrompt.prompt();
+        const response = await defferedPrompt.userChoice;
+        if(response.outcome === 'dismissed'){
+          console.log('El usuario cancelo la instalación')
+        }
+      }
+    });
 })
 
